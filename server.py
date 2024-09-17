3e0a2ac043da446d74804806ad0daa48f603226e
WinterEffect <yishaigame999@gmail.com>
2024-09-17 16:24:16 +0300

    added sell buy end points

--- server.py
+++ server.py
@@ -3,9 +3,19 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/engine/health')
+@app.route('/engine/health', methods=['GET'])
 def healthcheck():
     return "Healthy"
+
+
+@app.route('/engine/sell', methods=['POST'])
+def sell():
+    return request.json
+
+
+@app.route('/engine/buy', methods=['POST'])
+def buy():
+    return request.json
 
 
 if __name__ == '__main__':
